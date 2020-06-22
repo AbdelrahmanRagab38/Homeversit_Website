8493d5659234dbd44c1c91ea83642efa776bf1f8
AbdelrahmanRagab38 <abdelrahmanRagab38@gmail.com>
2020-06-22 02:33:27 -1000

    first commit isa

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+$('') 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top-200 && cur_pos <= bottom-200) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+$('.filter a').click(function(e) {
+  e.preventDefault();
+  var a = $(this).attr('href');
+  a = a.substr(1);
+  $('.gallery a').each(function() {
+    if (!$(this).hasClass(a) && a != 'all')
+      $(this).addClass('hide');
+    else
+      $(this).removeClass('hide');
+  });
+
+});
+
